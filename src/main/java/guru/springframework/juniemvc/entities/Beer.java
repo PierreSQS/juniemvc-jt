0f6d24b935085e77e5c9c482304dd07ee037e19e
PierreSQS <nomo_messina@web.de>
2025-06-16 08:47:02 +0200

    Sec3_Chap15: step1-added a JPA Beer Entity

---
+++ src/main/java/guru/springframework/juniemvc/entities/Beer.java
@@ -0,0 +1,42 @@
+package guru.springframework.juniemvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
